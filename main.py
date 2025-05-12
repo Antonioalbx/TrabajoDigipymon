f43063c8ca33e314333399e0da42cd6cab8a1b60
Antonioalbx <aaj0014@alu.medac.es>
2025-05-12 16:34:36 +0200

    Terminación de la función menu en el main

--- main.py
+++ main.py
@@ -5,8 +5,14 @@
 
 
 def menu():
-    print("Elige una opcion" \
-          "1.Buscar Digipymon"
-          "2. ")
+    print("Elige una opcion") 
+    print("1.Buscar Digipymon")
+    print("2.Luchar contra entrenador")
+    print("3.Ir a la tienda")
+    print("4.Usar objetos" )
+    print("5.Consultar inventario" )
+    print("6.Consultar digypimon") 
+    print("7.Salir")
 
 
+menu()

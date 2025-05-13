b3171b0b50401ee65c61628698a7d7c14c51a093
Gongo18 <jmc0043@alu.medac.es>
2025-05-13 19:07:14 +0200

    clase_inventario
    Clase inventario del programa para añadir y descartar o eliminar objetos con su cantidad y nombre

---
+++ clase_inventario.py
@@ -0,0 +1,17 @@
+class Inventario:
+    def __init__(self):
+        self.objetos = {}
+        
+    def añadir_objeto(self, nombre, cantidad):
+        if nombre in self.objetos:
+            self.objetos[nombre] += cantidad
+        else: 
+            self.objetos[nombre] = cantidad       
+        
+    def usar_objeto(self,nombre):
+       self.objetos[nombre] - 1
+       
+       if self.objetos[nombre] == 0 :
+        del self.objetos[nombre]
+    
+       

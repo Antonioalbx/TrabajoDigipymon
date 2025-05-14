eb4ca1bdd80ac21c6d1660032c89ef172f8905e3
Gongo18 <jmc0043@alu.medac.es>
2025-05-14 18:34:51 +0200

    clase_enemigo
    Clase enemigo entera definida subida desde mi propia rama

--- clase_enemigo.py
+++ clase_enemigo.py
@@ -1,8 +1,8 @@
-class Enemigo:
-    def __init__(self, nombre):
+class Enemigo:"""Clase Enemigo definida """
+def __init__(self, nombre):
         self.nombre = nombre
         self.lista_digimons = []
         self.cantidad_digipymon = 0
-    def añadir_digipymon(self, digipymon):
+def añadir_digipymon(self, digipymon):
          self.lista_digimons.append(digipymon)
          self.cantidad_digipymon += 1

95c376ac8113033eb9c2c44d75a721416bc4e306
Antonioalbx <aaj0014@alu.medac.es>
2025-05-14 18:15:14 +0200

    Creación y terminada la tienda

--- main.py
+++ main.py
@@ -8,6 +8,39 @@ from clase_digipymon import Digipymon
 from clase_inventario import Inventario
 
 
+def digishop(jugador, inventario):
+    salir_tienda = True
+    while salir_tienda:
+        print("Elige que objeto desea comprar")
+        print("1.Digipyballs -----> 5 digicoins")
+        print("2.Cocaina -------> 3 digicoins -----> 10 puntos de vida")
+        print("3.MK677 -------> 4 digicoins -----> 7 untos de ataque")
+        print("4.Salir")
+        opciontienda = input()
+        if opciontienda == 1:
+            if jugador.digicoins >= 5:
+                jugador.digicoins - 5
+                inventario.añadir_objeto("Digipyballs", 1)
+            else:
+                print("Eres un pobre")
+        if opciontienda == 2:
+            if jugador.digicoins >= 3:
+                jugador.digicoins - 3
+                inventario.añadir_objeto("Cocaina", 1)
+            else:
+                print("Eres un pobre")
+        if opciontienda == 3:
+            if jugador.digicoins >= 4:
+                jugador.digicoins - 4
+                inventario.añadir_objeto("MK677", 1)
+            else:
+                print("Eres un pobre")
+        if opciontienda == 4:
+            print("Estas saliendo de la tienda")
+            time.sleep(5)
+            salir_tienda = False
+            
+        
 
 def generar_digipymon_aleatorio():
     vida = random.radiant(10 , 20)
@@ -29,11 +62,13 @@ def combate():
     print("Te toca enfrentarte a " + enemigos.nombre)
     opcionpelea = input("¿Quieres pelear contra el? si/no")
     if opcionpelea == "si":
+        
 
+        
     
 
-
-
+    
+    
 def menu():
     print("Elige una opcion") 
     print("1.Buscar Digipymon")
@@ -43,6 +78,10 @@ def menu():
     print("5.Consultar inventario" )
     print("6.Consultar digypimon") 
     print("7.Salir")
+    
+    return menu
 
-
+def main():
+    
+    jugador = Jugador(lista_nombre.Obtener_nombre_entrenador)
     
